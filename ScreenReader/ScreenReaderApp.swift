19ebc0aa8c719edbf73816f98be76328c82eaa75
daniel <djs66256@163.com>
2025-03-27 16:24:08 +0800

    Initial Commit

---
+++ ScreenReader/ScreenReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenReaderApp.swift
+//  ScreenReader
+//
+//  Created by daniel on 2025/3/27.
+//
+
+import SwiftUI
+
+@main
+struct ScreenReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
